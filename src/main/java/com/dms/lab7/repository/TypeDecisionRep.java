e55f05e5bfb17b13b56df1884a49a4a709c4c0df
Artem_Kunats <Artem_Kunats@epam.com>
2018-12-16 19:37:56 +0300

    fixed

--- src/main/java/com/dms/lab7/repository/TypeDecisionRep.java
+++ src/main/java/com/dms/lab7/repository/TypeDecisionRep.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface TypeDecisionRep extends JpaRepository<TypeDecision, Long> {
-    @Query(value = "select * from dual", nativeQuery = true)
+    @Query(value = "select * from DECISION", nativeQuery = true)
     List<TypeDecision> findAllByStateId(Long id);
 }
